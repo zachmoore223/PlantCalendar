71f619f63b1e07c3b13dfef52756808c6604349a
Josef <josef.n.pohlmann@gmail.com>
2023-03-28 10:26:14 -0400

    first commit

---
+++ src/main/java/com/HydroHomies/PlantCalendar/PlantCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.HydroHomies.PlantCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HydroHomies/PlantCalendar/PlantCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HydroHomies.PlantCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
