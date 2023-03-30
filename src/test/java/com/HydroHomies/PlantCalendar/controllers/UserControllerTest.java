b5992944d0c90b02b33bfc229dee8ce206fb3dc8
Thomas <tmartin861@gmail.com>
2023-03-30 15:25:32 -0400

    Changed ManytoMany and created new test

--- src/test/java/com/HydroHomies/PlantCalendar/controllers/UserControllerTest.java
+++ src/test/java/com/HydroHomies/PlantCalendar/controllers/UserControllerTest.java
@@ -163,9 +163,86 @@ class UserControllerTest {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    public void addTwoUsers() throws Exception {
+        //create a new user and plant
+        User testUser = new User ("Tester");
+        User testUser2 = new User ("Tester2");
+        Plant testPlant = new Plant("Test", 1, "Low", "High", true, "testURL");
+        testPlant.setId(1);
+
+        //post user to database
+        mvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testUser)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testUser)))
+                .andDo(MockMvcResultHandlers.print());
+
+        // Added second user to test
+        mvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testUser2)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testUser2)))
+                .andDo(MockMvcResultHandlers.print());
+
+        //make sure user is in the database
+        mvc.perform(MockMvcRequestBuilders.get("/api/users/" + testUser.getUsername())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testUser)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testUser)))
+                .andDo(MockMvcResultHandlers.print());
+
+        // Make sure the second user is in the database
+        mvc.perform(MockMvcRequestBuilders.get("/api/users/" + testUser2.getUsername())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testUser2)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testUser2)))
+                .andDo(MockMvcResultHandlers.print());
+
+        //post the plant to the database
+        mvc.perform(MockMvcRequestBuilders.post("/api/plants")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testPlant)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testPlant)))
+                .andDo(MockMvcResultHandlers.print());
+        //make sure the plant is in the database
+        mvc.perform(MockMvcRequestBuilders.get("/api/plants/" + testPlant.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getJsonContent(testPlant)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(getJsonContent(testPlant)))
+                .andDo(MockMvcResultHandlers.print());
+        //add the plant in the database to the user's collection
+        mvc.perform(MockMvcRequestBuilders.put("/api/users/" + testUser.getUsername() + "/plantCollection/" + testPlant.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+        //add the plant in the database to the second user's collection
+        mvc.perform(MockMvcRequestBuilders.put("/api/users/" + testUser2.getUsername() + "/plantCollection/" + testPlant.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
 
 
     private static String getJsonContent(Object o) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(o);
     }
+
+
 }
