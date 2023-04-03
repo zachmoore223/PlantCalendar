067823fc8911c8e0c7fa87919b766abe9344991b
Zach Moore <zachmoore223@gmail.com>
2023-04-03 14:23:56 -0400

    calendar working

--- frontend/src/components/CreateICS.js
+++ frontend/src/components/CreateICS.js
@@ -1,43 +1,34 @@
 import React from "react";
+import User from "./User";
 import { createEvent } from "ics";
 import { saveAs } from "file-saver";
 
-export default function CreateICS() {
-  // Set up event schedule details
-  const event = {
-    start: [2018, 5, 30, 6, 30],
-    duration: { hours: 6, minutes: 30 },
-    title: "Bolder Boulder",
-    description: "Annual 10-kilometer run in Boulder, Colorado",
-    location: "Folsom Field, University of Colorado (finish line)",
-    url: "http://www.bolderboulder.com/",
-    geo: { lat: 40.0095, lon: 105.2669 },
-    categories: ["10k races", "Memorial Day Weekend", "Boulder CO"],
-    status: "CONFIRMED",
-    busyStatus: "BUSY",
-    organizer: { name: "Admin", email: "Race@BolderBOULDER.com" },
-    attendees: [
-      {
-        name: "Adam Gibbons",
-        email: "adam@example.com",
-        rsvp: true,
-        partstat: "ACCEPTED",
-        role: "REQ-PARTICIPANT"
-      },
-      {
-        name: "Brittany Seaton",
-        email: "brittany@example2.org",
-        dir: "https://linkedin.com/in/brittanyseaton",
-        role: "OPT-PARTICIPANT"
-      }
-    ]
-  };
+export default function CreateICS({user}) {
+const ics = require('ics')
+
+const { error, value } = ics.createEvents([
+  {
+    title: 'Lunch',
+    start: [2018, 1, 15, 12, 15],
+    duration: { minutes: 45 }
+  },
+  {
+    title: 'Dinner',
+    start: [2018, 1, 15, 12, 15],
+    duration: { hours: 1, minutes: 30 }
+  }
+])
+
+if (error) {
+  console.log(error)
+  return
+}
+
+console.log(value);
 
   const handleSave = () => {
-    createEvent(event, (error, value) => {
       const blob = new Blob([value], { type: "text/plain;charset=utf-8" });
       saveAs(blob, "event-schedule.ics");
-    });
   };
 
   return (
