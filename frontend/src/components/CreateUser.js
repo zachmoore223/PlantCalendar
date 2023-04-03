82b3336c02190dbdaf1984ac01c2a9e6244b1e85
Zach Moore <zachmoore223@gmail.com>
2023-04-03 00:01:33 -0400

    code reformat

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,86 +1,80 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function CreateUser() {
-  const [text, setText] = useState('');
-  const [status, setStatus] = useState('typing');
-  const [users, setUsers] = useState([])
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing");
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-          const periodicallyFetch = setInterval(
-             () => fetch("http://localhost:8080/api/users")
-                        .then(res => res.json())
-                        .then(response => setUsers(response)),
-             1000);
-          return () => clearInterval(periodicallyFetch)
-      }, [])
-
+    const periodicallyFetch = setInterval(
+      () =>
+        fetch("http://localhost:8080/api/users")
+          .then((res) => res.json())
+          .then((response) => setUsers(response)),
+      1000
+    );
+    return () => clearInterval(periodicallyFetch);
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setStatus('sending');
+    setStatus("sending");
     await sendMessage(text);
     /*if text is empty when user submits reset status to typing
     and do not add the blank user to the API */
-    if(text == ""){
-        setStatus('typing')
+    if (text == "") {
+      setStatus("typing");
     } else {
-        postNewUser(text);
-        setStatus('sent');
+      postNewUser(text);
+      setStatus("sent");
     }
-
-
   }
 
-  const isSending = status === 'sending';
-  const isSent = status === 'sent';
+  const isSending = status === "sending";
+  const isSent = status === "sent";
 
   if (isSent) {
-    return     <div>
-                    <p>User {text} created.</p>
-               </div>
+    return (
+      <div>
+        <p>User {text} created.</p>
+      </div>
+    );
   }
 
   return (
-  <div>
-  <br />
-    <form onSubmit={handleSubmit}>
-
+    <div>
+      <br />
+      <form onSubmit={handleSubmit}>
         <input
-            disabled={isSending}
-            value={text}
-            onChange={e => setText(e.target.value)}
+          disabled={isSending}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
         <br />
 
-        <button
-             disabled={isSending}
-        >
-          Create User
-        </button>
+        <button disabled={isSending}>Create User</button>
 
         {isSending && <p>Creating User... </p>}
-
-
-    </form>
+      </form>
     </div>
   );
 }
 
 // Pretend to send a message.
 function sendMessage(text) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, 3000);
   });
 }
 
-function postNewUser (text) {
-fetch('http://localhost:8080/api/users', {
-    method: 'POST',
+function postNewUser(text) {
+  fetch("http://localhost:8080/api/users", {
+    method: "POST",
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ "username": text })
-})
-   .then(response => response.json())
-   .then(response => console.log(JSON.stringify(response)))
+    body: JSON.stringify({ username: text }),
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)));
 }
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,98 +1,95 @@
-import { useState, useEffect } from 'react';
-import Plant from './Plant';
+import { useState, useEffect } from "react";
+import Plant from "./Plant";
 
 export default function User() {
-  const [text, setText] = useState('');
-  const [status, setStatus] = useState('typing');
-  const [users, setUsers] = useState([])
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing");
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-          const periodicallyFetch = setInterval(
-             () => fetch("http://localhost:8080/api/users")
-                        .then(res => res.json())
-                        .then(response => setUsers(response)),
-             1000);
-          return () => clearInterval(periodicallyFetch)
-      }, [])
-
+    const periodicallyFetch = setInterval(
+      () =>
+        fetch("http://localhost:8080/api/users")
+          .then((res) => res.json())
+          .then((response) => setUsers(response)),
+      1000
+    );
+    return () => clearInterval(periodicallyFetch);
+  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setStatus('sending');
+    setStatus("sending");
     await sendMessage(text);
 
     //if text is empty when user submits reset status to typing
     let userExists = makeSureUserExists(users, text);
 
-    if(text == "" || userExists == false){
-        setStatus('typing')
+    if (text == "" || userExists == false) {
+      setStatus("typing");
     } else {
-        setStatus('sent');
+      setStatus("sent");
     }
-
   }
 
-  const isSending = status === 'sending';
-  const isSent = status === 'sent';
+  const isSending = status === "sending";
+  const isSent = status === "sent";
 
   if (isSent) {
-    return     <div>
-                   {users
-                   .filter(user => user.username == text)
-                   .map(user => (
-                        <div  key={user.username}>
-                            <h1> Welcome {user.username}</h1>
-                            <Plant user={user}/>
-                        </div>
-                   ))}
-
-               </div>
+    return (
+      <div>
+        {users
+          .filter((user) => user.username == text)
+          .map((user) => (
+            <div key={user.username}>
+              <h1> Welcome {user.username}</h1>
+              <Plant user={user} />
+            </div>
+          ))}
+      </div>
+    );
   }
 
   return (
-  <div>
-  <h1>Welcome</h1>
-    <form onSubmit={handleSubmit}>
-      <h3>Login Here:</h3>
-      <input
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
+    <div>
+      <h1>Welcome</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>Login Here:</h3>
+        <input
+          disabled={isSending}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
 
-      <br />
+        <br />
 
-      <button
-        disabled={isSending}
-        type="submit"
-      >
-        Login
-      </button>
+        <button disabled={isSending} type="submit">
+          Login
+        </button>
 
-      {isSending && <p>Logging in...</p>}
-
-    </form>
-   </div>
+        {isSending && <p>Logging in...</p>}
+      </form>
+    </div>
   );
 }
 
 // Pretend to send a message.
 function sendMessage(text) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, 2000);
   });
 }
 
-function makeSureUserExists (users, text) {
-   console.log(users);
-   let check = false;
+function makeSureUserExists(users, text) {
+  console.log(users);
+  let check = false;
 
-   for (let i = 0; i < users.length; i++){
-      if (users[i].username == text){
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == text) {
       check = true;
-      }
-   }
+    }
+  }
 
-    return check;
+  return check;
 }
 
 //function getUser ({user}){
