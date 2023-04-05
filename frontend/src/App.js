d1c8c28c80cf98b8f936c95ebf745e419c606eca
Zach Moore <zachmoore223@gmail.com>
2023-04-05 14:08:47 -0400

    HH logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import User from './components/User';
 import Plant from './components/Plant';
 import CreateUser from './components/CreateUser';
+import hydrohomieLogo from './hydrohomieLogo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={hydrohomieLogo} width="750px"/>
         <User />
       </header>
     </div>
