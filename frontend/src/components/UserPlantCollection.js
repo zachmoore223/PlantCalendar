6964a620d9d2791883c69caf043139e22b9b19fb
Zach Moore <zachmoore223@gmail.com>
2023-04-05 14:49:07 -0400

    refactored and added notes

---
+++ frontend/src/components/UserPlantCollection.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import User from "./User";
+import CreateICS from "./CreateICS";
+
+export default function UserPlantCollection({ user }) {
+const [plants, setPlants] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/api/plants")
+            .then((res) => res.json())
+            .then((response) => setPlants(response));
+    }, [user]);
+
+    return (
+        user.allPlants.map((plant) => (
+            <td key={plant.id}>
+                <p>{plant.name} &nbsp; <button onClick={() => removePlant(user, plant)}>X</button> &nbsp;| &nbsp; </p>
+            </td>
+        ))
+    );
+}
+
+
+function removePlant(user, plant) {
+    fetch(
+        "http://localhost:8080/api/users/" +
+        user.username +
+        "/plantCollection/" +
+        plant.id,
+        { method: "DELETE", cache: "default" }
+    );
+}
+
+
+
+
