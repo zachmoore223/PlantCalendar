8942e271c0f6febd5f3d0872db8e6938a840c6d4
Josef <josef.n.pohlmann@gmail.com>
2023-04-11 14:03:39 -0400

    api for schedule change

--- src/test/java/com/HydroHomies/PlantCalendar/entities/PlantTest.java
+++ src/test/java/com/HydroHomies/PlantCalendar/entities/PlantTest.java
@@ -8,13 +8,19 @@ class PlantTest {
 
     @Test
     public void testEntity() {
-        Plant test = new Plant("Test", "FREQ=DAILY;INTERVAL=2", "Low", "High", true, "url");
+        Plant test = new Plant("Test", "FREQ=DAILY;INTERVAL=2;BYDAY=SU", "Low", "High", true, "url");
         assertEquals("Test", test.getName());
-        assertEquals("FREQ=DAILY;INTERVAL=2", test.getWateringSchedule());
+        assertEquals("FREQ=DAILY;INTERVAL=2;BYDAY=SU", test.getWateringSchedule());
         assertEquals("Low", test.getWaterAmount());
         assertEquals("High", test.getLightLevel());
         assertEquals(true, test.isPetFriendly());
         assertEquals("url", test.getImgURL());
     }
 
+    @Test
+    public void testChange() {
+        Plant test = new Plant("Test", "FREQ=DAILY;INTERVAL=2;BYDAY=SU", "Low", "High", true, "url");
+        test.changeWateringSchedule("MO");
+        assertEquals("FREQ=DAILY;INTERVAL=2;BYDAY=MO", test.getWateringSchedule());
+    }
 }
